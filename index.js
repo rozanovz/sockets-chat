15e72026a9d141699bc20098e5bc8406f8807622
rozanovz <rozanovz@gmail.com>
2016-07-12 14:13:01 +0300

    multirooms created

--- index.js
+++ index.js
@@ -9,13 +9,52 @@ app.get('', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
-  });
-});
+var usernames = {};
+var rooms = ['Lobby'];
 
+io.sockets.on('connection', function(socket) {
+  socket.on('create', function(room) {
+    rooms.push(room);
+    socket.emit('updaterooms', rooms, socket.room);
+  });
+
+  socket.on('sendchat', function(data) {
+  	usernames[socket.username].messages.push({txt: data, date: new Date()});
+    io.sockets["in"](socket.room).emit('updatechat', socket.username, data)
+  });
+
+	socket.on('disconnect', function() {
+    delete usernames[socket.username];
+    io.sockets.emit('udateusers', usernames);
+    io.sockets.emit('updateusers', usernames);
+    socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+    socket.leave(socket.room);
+  });
+
+  socket.on('switchRoom', function(newroom) {
+    var oldroom;
+    oldroom = socket.room;
+    socket.leave(socket.room);
+    socket.join(newroom);
+    socket.emit('updatechat', 'SERVER', 'you have connected to ' + newroom);
+    socket.broadcast.to(oldroom).emit('updatechat', 'SERVER', socket.username + ' has left this room');
+    socket.room = newroom;
+    socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username + ' has joined this room');
+    io.sockets.emit('updaterooms', rooms, newroom);
+  });
+
+  socket.on('adduser', function(username) {
+    socket.username = username;
+    socket.room = 'Lobby';
+    usernames[username] = {name: username, messages:[]};
+    socket.join('Lobby');
+    socket.emit('updatechat', 'SERVER', 'you have connected to Lobby');
+    socket.broadcast.to('Lobby').emit('updatechat', 'SERVER', username + ' has connected to this room');
+    socket.emit('updaterooms', rooms, 'Lobby');
+    io.sockets.emit('udateusers', usernames);
+  });
+
+});
 http.listen(3000, ()=>{
 	console.log('listening on 3000');
 });
--- public/app.js
+++ public/app.js
@@ -1,10 +1,86 @@
 var socket = io();
-$('form').submit(function(){
-    socket.emit('chat message', $('#m').val());
-    $('#m').val('');
-    return false;
+
+function switchRoom(room){
+    socket.emit('switchRoom', room);
+}
+
+function dateGet(val) {
+	if(val) {
+		return (
+			new Date(val).getDate() 	  + '.'   + 
+			(new Date(val).getMonth()+1)  + '.'   +
+			new Date(val).getFullYear()   + ' @ ' + 
+			new Date(val).getHours() 	  + ':'   + 
+			new Date(val).getMinutes()    + ':'   + 
+			new Date(val).getSeconds()
+		);
+	} else {
+		return (
+			new Date().getDate() 	  + '.'   + 
+			(new Date().getMonth()+1) + '.'   +
+			new Date().getFullYear()  + ' @ ' + 
+			new Date().getHours() 	  + ':'   + 
+			new Date().getMinutes()   + ':'   + 
+			new Date().getSeconds()
+		);
+	}
+}
+
+socket.on('connect', function(){
+    socket.emit('adduser', prompt("What's your name: "));
 });
 
-socket.on('chat message', function(msg){
-    $('#messages').append($('<li>').text(msg));
+socket.on('updatechat', function (username, data) {
+    $('#conversation').append('<li>'+'('+dateGet()+') '+ username +':</b> ' + data + '</li>');
+});
+
+socket.on('updaterooms', function (rooms, current_room) {
+    $('#rooms').empty();
+    $.each(rooms,function(key, value) {
+        (value == current_room) 
+        	? $('#rooms').append('<li><h4><i class="glyphicon glyphicon-headphones"></i> ' + value + '</h4></li>') 
+        		: $('#rooms').append('<li><a href="#" onclick="switchRoom(\''+value+'\')"><h4><i class="glyphicon glyphicon-headphones"></i> ' + value + '</h4></a></li>');
+    });
+});
+
+socket.on('udateusers', function (users) {
+    $('#users').empty();
+
+    var userMessages = [];
+
+    $.each(users,function(key, value) {
+       $('#users').append('<li><h4><i class="glyphicon glyphicon-user"></i> ' + value.name + '</h4></li>');
+
+       if(value.messages.length > 0){
+		   	value.messages.forEach(function(key){
+		   		key.txt = '<li>'+'('+dateGet(key.date)+') '+  value.name +':</b> ' +  key.txt + '</li>';
+		   		userMessages.push(key);
+		   	});
+       }
+    });
+
+    userMessages.sort().forEach(function(key){
+    	$('#conversation').append(key.txt);
+    })
+});
+
+$(function(){
+    $('#datasend').click( function() {
+        var message = $('#data').val();
+        $('#data').val('');
+        socket.emit('sendchat', message);
+    });
+
+    $('#roombutton').click(function(){
+        var name = $('#roomname').val();
+        $('#roomname').val('');
+        socket.emit('create', name)
+    });
+
+    $('#data').keypress(function(e) {
+        if(e.which == 13) {
+            $(this).blur();
+            $('#datasend').focus().click();
+        }
+    });
 });
