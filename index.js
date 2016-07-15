bf135469be9a3e87373faa0e842e0aa76a8dcf9c
rozanovz <rozanovz@gmail.com>
2016-07-15 16:56:18 +0300

    added some refactoring

--- index.js
+++ index.js
@@ -11,10 +11,15 @@ const path = require('path');
 const mime = require('mime');
 
 
+app.set("views", __dirname + "/views");
+app.set("view engine", "jade");
+app.use(express.static(__dirname + '/public'));
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/socketsChat', (err) => {
     if (err) { console.log(err) }
 });
+
+
 
 const User = mongoose.model('User', {
     name: String,
@@ -33,31 +38,19 @@ let uploadedFiles = {};
 let rooms = ['Lobby'];
 let usernames = {};
 
+
 User.find().then( res => res.forEach( key => usernames[key._id] = {name: key.name, messages: key.messages, id: key._id}));
 Room.find().then( res => res.forEach( key => rooms = [...rooms, key.name] ));
-
-
-app.set("views", __dirname + "/views");
-app.set("view engine", "jade");
-app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
 	res.render("index");
 });
 
 app.get('/download', (req, res) => {
-  let url_parts = url.parse(req.url, true);
-  let query = url_parts.query;
-
-  let file = `${__dirname}/tmp/${query.name}`;
-  let filename = path.basename(file);
-  let mimetype = mime.lookup(file);
-
-  res.setHeader('Content-disposition', 'attachment; filename=' + filename);
-  res.setHeader('Content-type', mimetype);
-  
-  let filestream = fs.createReadStream(file);
-  filestream.pipe(res);
+  let file = `${__dirname}/tmp/${url.parse(req.url, true).query.name}`;
+  res.setHeader('Content-disposition', `attachment; filename=${path.basename(file)}`);
+  res.setHeader('Content-type', mime.lookup(file));
+  fs.createReadStream(file).pipe(res);
 });
 
 io.sockets.on('connection', socket => {
@@ -70,8 +63,8 @@ io.sockets.on('connection', socket => {
   socket.on('disconnect', () => {
     delete usernames[socket.username];
     io.sockets.emit('updateusers', usernames);
-    socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
-    socket.leave(socket.room);
+    socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected')
+          .leave(socket.room);
   });
 
 
@@ -94,42 +87,48 @@ io.sockets.on('connection', socket => {
   });
 
   socket.on('switchRoom', newroom => {
-    socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username + ' has left this room');
-    socket.leave(socket.room);
-   
-    socket.join(newroom);
-    socket.emit('updatechat', 'SERVER', 'you have connected to ' + newroom);
+    socket.broadcast.to(socket.room)
+          .emit('updatechat', 'SERVER', socket.username + ' has left this room')
+          .leave(socket.room)
+          .join(newroom)
+          .emit('updatechat', 'SERVER', 'you have connected to ' + newroom)
+          .broadcast.to(newroom)
+          .emit('updatechat', 'SERVER', socket.username + ' has joined this room')
+          .emit('updaterooms', rooms, newroom, socket.username);
     socket.room = newroom;
-
-    socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username + ' has joined this room');
-    socket.emit('updaterooms', rooms, newroom, socket.username);
   });
 
   socket.on('sendchat', data => {
   	usernames[socket._id].messages = [...usernames[socket._id].messages, {txt: data, date: new Date()}];
-    User.update({'_id': socket._id}, {'name': socket.name, 'messages': usernames[socket._id].messages}).then((res) => {
+    
+    User.update({'_id': socket._id}, {'name': socket.name, 'messages': usernames[socket._id].messages})
+    .then(res => {
       io.sockets["in"](socket.room).emit('updatechat', socket.username, data)
-    }).catch((err) => {
+    }).catch(err => {
       console.log(err);
     });
   });
 
   socket.on('adduser', username => {
     username = username ? username : `user #${usernames.length}`;
-    console.log(username);
+
     socket.username = username;
     socket.room = 'Lobby';
+   
     let newUser = new User({ name: username });
-    newUser.save().then((res) => {
+    
+    newUser.save().then(res => {
       usernames[res._id] = {name: res.name, messages:[], id: res._id};
+      
       socket._id = res._id;
-      socket.join('Lobby');
-      socket.emit('updatechat', 'SERVER', 'you have connected to Lobby');
-      socket.broadcast.to('Lobby').emit('updatechat', 'SERVER', res.name + ' has connected to this room');
-      socket.emit('updaterooms', rooms, 'Lobby');
+      socket.join('Lobby')
+            .emit('updatechat', 'SERVER', 'you have connected to Lobby')
+            .broadcast.to('Lobby').emit('updatechat', 'SERVER', res.name + ' has connected to this room')
+            .emit('updaterooms', rooms, 'Lobby');
+
       io.sockets.emit('updateusers', usernames);
     })
-    .catch((err) => {
+    .catch(err => {
       console.log(err);
     });    
   });
